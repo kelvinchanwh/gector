123f1889d89c4cfd5e698def472ca878ed002a0d
Kelvin Chan <kelvin.chanwh@hotmail.com>
2023-03-07 00:28:08 +0000

    Added flexible evaluation for language

--- evaluate.sh
+++ evaluate.sh
@@ -1,31 +1,31 @@
 source venv/bin/activate
 
 eval python predict.py --model_path models/output/$1/best.th \
-                --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/annotated/annotated.en.zh.src.txt \
-                --output_file data/corpus/eval/annotated/annotated.en.zh.$1.hyp.txt \
+                --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/annotated/annotated.en.$2.src.txt \
+                --output_file data/corpus/eval/annotated/annotated.en.$2.$1.hyp.txt \
                 --transformer_model xlm-roberta
 
 eval python predict.py --model_path models/output/$1/best.th \
-                --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/lang8/lang8.en.zh.src.txt \
-                --output_file data/corpus/eval/lang8/lang8.en.zh.$1.hyp.txt \
+                --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/lang8/lang8.en.$2.src.txt \
+                --output_file data/corpus/eval/lang8/lang8.en.$2.$1.hyp.txt \
                 --transformer_model xlm-roberta
 
 source errant_env/bin/activate
 
 
-eval errant_parallel -orig data/corpus/eval/lang8/lang8.en.zh.src.txt -cor data/corpus/eval/lang8/lang8.en.zh.$1.hyp.txt -out data/corpus/eval/lang8/lang8.en.zh.$1.hyp.m2
-eval errant_parallel -orig data/corpus/eval/annotated/annotated.en.zh.src.txt -cor data/corpus/eval/annotated/annotated.en.zh.$1.hyp.txt -out data/corpus/eval/annotated/annotated.en.zh.$1.hyp.m2
+eval errant_parallel -orig data/corpus/eval/lang8/lang8.en.$2.src.txt -cor data/corpus/eval/lang8/lang8.en.$2.$1.hyp.txt -out data/corpus/eval/lang8/lang8.en.$2.$1.hyp.m2
+eval errant_parallel -orig data/corpus/eval/annotated/annotated.en.$2.src.txt -cor data/corpus/eval/annotated/annotated.en.$2.$1.hyp.txt -out data/corpus/eval/annotated/annotated.en.$2.$1.hyp.m2
 
 echo $1 > results.$1.txt
 
 echo "\"Lang-8 CS\"    Span    GEC" >> results.$1.txt
-eval errant_compare -hyp data/corpus/eval/lang8/lang8.en.zh.$1.hyp.m2 -ref data/corpus/eval/lang8/lang8.en.zh.m2 -cat 3 >> results.$1.txt
+eval errant_compare -hyp data/corpus/eval/lang8/lang8.en.$2.$1.hyp.m2 -ref data/corpus/eval/lang8/lang8.en.$2.m2 -cat 3 >> results.$1.txt
 
 echo "\"Lang-8 CS\"    Span    GED" >> results.$1.txt
-eval errant_compare -hyp data/corpus/eval/lang8/lang8.en.zh.$1.hyp.m2 -ref data/corpus/eval/lang8/lang8.en.zh.m2 -cat 3 -ds >> results.$1.txt
+eval errant_compare -hyp data/corpus/eval/lang8/lang8.en.$2.$1.hyp.m2 -ref data/corpus/eval/lang8/lang8.en.$2.m2 -cat 3 -ds >> results.$1.txt
 
 echo "\"Annotated CS\"    Span    GEC" >> results.$1.txt
-eval errant_compare -hyp data/corpus/eval/annotated/annotated.en.zh.$1.hyp.m2 -ref data/corpus/eval/annotated/annotated.en.zh.m2 -cat 3 >> results.$1.txt
+eval errant_compare -hyp data/corpus/eval/annotated/annotated.en.$2.$1.hyp.m2 -ref data/corpus/eval/annotated/annotated.en.$2.m2 -cat 3 >> results.$1.txt
 
 echo "\"Annotated CS\"    Span    GED" >> results.$1.txt
-eval errant_compare -hyp data/corpus/eval/annotated/annotated.en.zh.$1.hyp.m2 -ref data/corpus/eval/annotated/annotated.en.zh.m2 -cat 3 -ds >> results.$1.txt
+eval errant_compare -hyp data/corpus/eval/annotated/annotated.en.$2.$1.hyp.m2 -ref data/corpus/eval/annotated/annotated.en.$2.m2 -cat 3 -ds >> results.$1.txt
