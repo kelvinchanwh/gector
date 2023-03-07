2154a8baf090eec13e3c951c7d141f4951d04aa7
Kelvin Chan <kwhc2@cam.ac.uk>
2023-03-07 21:57:53 +0000

    Rearrange eval folder

--- evaluate.sh
+++ evaluate.sh
@@ -3,7 +3,7 @@ source venv/bin/activate
 if [ ! -f data/corpus/eval/annotated/annotated.en.$2.$1.hyp.txt ]
 then
     eval python predict.py --model_path models/output/$1/best.th \
-                    --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/annotated/annotated.en.$2.src.txt \
+                    --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/annotated/gold/annotated.en.$2.src.txt \
                     --output_file data/corpus/eval/annotated/annotated.en.$2.$1.hyp.txt \
                     --transformer_model xlm-roberta
 fi
@@ -11,7 +11,7 @@ fi
 if [ ! -f data/corpus/eval/lang8/lang8.en.$2.$1.hyp.txt ]
 then
     eval python predict.py --model_path models/output/$1/best.th \
-                    --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/lang8/lang8.en.$2.src.txt \
+                    --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/lang8/gold/lang8.en.$2.src.txt \
                     --output_file data/corpus/eval/lang8/lang8.en.$2.$1.hyp.txt \
                     --transformer_model xlm-roberta
 fi
@@ -19,7 +19,7 @@ fi
 if [ ! -f data/corpus/eval/bea19/bea19.dev.en.$1.hyp.txt ]
 then
 	eval python predict.py --model_path models/output/$1/best.th \
-                --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/bea19/bea19.dev.en.src.txt \
+                --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/bea19/gold/bea19.dev.en.src.txt \
                 --output_file data/corpus/eval/bea19/bea19.dev.en.$1.hyp.txt \
                 --transformer_model xlm-roberta
 fi
@@ -28,35 +28,35 @@ source errant_env/bin/activate
 
 echo $1 > results.$1.txt
 
-if [ -f data/corpus/eval/lang8/lang8.en.$2.$1.hyp.m2 ]
+if [ -f data/corpus/eval/lang8/lang8.en.$2.$1.hyp.txt ]
 then
-    eval errant_parallel -orig data/corpus/eval/lang8/lang8.en.$2.src.txt -cor data/corpus/eval/lang8/lang8.en.$2.$1.hyp.txt -out data/corpus/eval/lang8/lang8.en.$2.$1.hyp.m2
+    eval errant_parallel -orig data/corpus/eval/lang8/gold/lang8.en.$2.src.txt -cor data/corpus/eval/lang8/lang8.en.$2.$1.hyp.txt -out data/corpus/eval/lang8/lang8.en.$2.$1.hyp.m2
 
     echo "\"Lang-8 CS\"    $2    Span    GEC" >> results.$2.$1.txt
-    eval errant_compare -hyp data/corpus/eval/lang8/lang8.en.$2.$1.hyp.m2 -ref data/corpus/eval/lang8/lang8.en.$2.m2 -cat 3 >> results.$2.$1.txt
+    eval errant_compare -hyp data/corpus/eval/lang8/lang8.en.$2.$1.hyp.m2 -ref data/corpus/eval/lang8/gold/lang8.en.$2.m2 -cat 3 >> results.$2.$1.txt
 
     echo "\"Lang-8 CS\"    $2    Span    GED" >> results.$2.$1.txt
-    eval errant_compare -hyp data/corpus/eval/lang8/lang8.en.$2.$1.hyp.m2 -ref data/corpus/eval/lang8/lang8.en.$2.m2 -cat 3 -ds >> results.$2.$1.txt
+    eval errant_compare -hyp data/corpus/eval/lang8/lang8.en.$2.$1.hyp.m2 -ref data/corpus/eval/lang8/gold/lang8.en.$2.m2 -cat 3 -ds >> results.$2.$1.txt
 fi
 
-if [ -f data/corpus/eval/annotated/annotated.en.$2.$1.hyp.m2 ]
+if [ -f data/corpus/eval/annotated/annotated.en.$2.$1.hyp.txt ]
 then
-    eval errant_parallel -orig data/corpus/eval/annotated/annotated.en.$2.src.txt -cor data/corpus/eval/annotated/annotated.en.$2.$1.hyp.txt -out data/corpus/eval/annotated/annotated.en.$2.$1.hyp.m2
+    eval errant_parallel -orig data/corpus/eval/annotated/gold/annotated.en.$2.src.txt -cor data/corpus/eval/annotated/annotated.en.$2.$1.hyp.txt -out data/corpus/eval/annotated/annotated.en.$2.$1.hyp.m2
 
     echo "\"Annotated CS\"    $2    Span    GEC" >> results.$2.$1.txt
-    eval errant_compare -hyp data/corpus/eval/annotated/annotated.en.$2.$1.hyp.m2 -ref data/corpus/eval/annotated/annotated.en.$2.m2 -cat 3 >> results.$2.$1.txt
+    eval errant_compare -hyp data/corpus/eval/annotated/annotated.en.$2.$1.hyp.m2 -ref data/corpus/eval/annotated/gold/annotated.en.$2.m2 -cat 3 >> results.$2.$1.txt
 
     echo "\"Annotated CS\"    $2    Span    GED" >> results.$2.$1.txt
-    eval errant_compare -hyp data/corpus/eval/annotated/annotated.en.$2.$1.hyp.m2 -ref data/corpus/eval/annotated/annotated.en.$2.m2 -cat 3 -ds >> results.$2.$1.txt
+    eval errant_compare -hyp data/corpus/eval/annotated/annotated.en.$2.$1.hyp.m2 -ref data/corpus/eval/annotated/gold/annotated.en.$2.m2 -cat 3 -ds >> results.$2.$1.txt
 fi
 
-if [ -f data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2 ]
+if [ -f data/corpus/eval/bea19/bea19.dev.en.$1.hyp.txt ]
 then
-    eval errant_parallel -orig data/corpus/eval/bea19/bea19.dev.en.src.txt -cor data/corpus/eval/bea19/bea19.dev.en.$1.hyp.txt -out data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2
+    eval errant_parallel -orig data/corpus/eval/bea19/gold/bea19.dev.en.src.txt -cor data/corpus/eval/bea19/bea19.dev.en.$1.hyp.txt -out data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2
 
     echo "\"BEA-19 Dev\"    EN    Span    GEC" >> results.$2.$1.txt
-    eval errant_compare -hyp data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2 -ref data/corpus/eval/bea19/bea19.dev.en.m2 -cat 3 >> results.$2.$1.txt
+    eval errant_compare -hyp data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2 -ref data/corpus/eval/bea19/gold/bea19.dev.en.m2 -cat 3 >> results.$2.$1.txt
 
     echo "\"BEA-19 Dev\"    EN    Span    GED" >> results.$2.$1.txt
-    eval errant_compare -hyp data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2 -ref data/corpus/eval/bea19/bea19.dev.en.m2 -cat 3 -ds >> results.$2.$1.txt
+    eval errant_compare -hyp data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2 -ref data/corpus/eval/bea19/gold/bea19.dev.en.m2 -cat 3 -ds >> results.$2.$1.txt
 fi
