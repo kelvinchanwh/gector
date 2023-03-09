979c544aac4cf84283e2774a19c9cc6c9d3cdc4e
Kelvin Chan <kwhc2@cam.ac.uk>
2023-03-09 00:35:05 +0000

    Updated for inference prediction

--- evaluate.sh
+++ evaluate.sh
@@ -11,7 +11,8 @@ then
         eval python predict.py --model_path models/output/$1/best.th \
                         --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/annotated/gold/annotated.en.$2.src.txt \
                         --output_file data/corpus/eval/annotated/annotated.en.$2.$1.hyp.txt \
-                        --transformer_model xlm-roberta
+                        --transformer_model xlm-roberta \
+                        --pre_translate 1
     fi
 
     if [ ! -f data/corpus/eval/lang8/lang8.en.$2.$1.hyp.txt ] && [ -f data/corpus/eval/lang8/gold/lang8.en.$2.src.txt ]
@@ -20,16 +21,8 @@ then
         eval python predict.py --model_path models/output/$1/best.th \
                         --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/lang8/gold/lang8.en.$2.src.txt \
                         --output_file data/corpus/eval/lang8/lang8.en.$2.$1.hyp.txt \
-                        --transformer_model xlm-roberta
-    fi
-
-    if [ ! -f data/corpus/eval/bea19/bea19.dev.en.$1.hyp.txt ] && [ -f data/corpus/eval/bea19/gold/bea19.dev.en.src.txt ]
-    then
-        echo "Predicting bea19.dev.en.$2.$1.hyp.txt"
-        eval python predict.py --model_path models/output/$1/best.th \
-                    --vocab_path models/output/$1/vocabulary --input_file data/corpus/eval/bea19/gold/bea19.dev.en.src.txt \
-                    --output_file data/corpus/eval/bea19/bea19.dev.en.$1.hyp.txt \
-                    --transformer_model xlm-roberta
+                        --transformer_model xlm-roberta \
+                        --pre_translate 1
     fi
 
     source errant_env/bin/activate
@@ -94,34 +87,4 @@ then
 
         rm "$temp_file"*
     fi
-
-    if [ -f data/corpus/eval/bea19/bea19.dev.en.$1.hyp.txt ]
-    then
-        if [ ! -f data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2 ]
-        then
-            echo "Evaluating bea19.dev.en.$2.$1.hyp.txt"
-            eval errant_parallel -orig data/corpus/eval/bea19/gold/bea19.dev.en.src.txt -cor data/corpus/eval/bea19/bea19.dev.en.$1.hyp.txt -out data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2
-
-            prefix="\"BEA-19 Dev\"    EN    Span    GEC   "
-            eval errant_compare -hyp data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2 -ref data/corpus/eval/bea19/gold/bea19.dev.en.m2 -cat 3 >> "$temp_file""1"
-            sed -i -e $prepattern_error -e :a -e $postpattern_error -e 'P;D' "$temp_file""1"
-            awk -v p="$prefix " '{print p $0}' "$temp_file""1" >> $error_file
-
-            eval errant_compare -hyp data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2 -ref data/corpus/eval/bea19/gold/bea19.dev.en.m2 >> "$temp_file""2"
-            sed -i -e $prepattern_results -e :a -e $postpattern_results -e 'P;D' "$temp_file""2"
-            awk -v p="$prefix " '{print p $0}' "$temp_file""2" >> $results_file
-
-            prefix="\"BEA-19 Dev\"    EN    Span    GED    "
-            eval errant_compare -hyp data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2 -ref data/corpus/eval/bea19/gold/bea19.dev.en.m2 -cat 3 -ds > "$temp_file""3"
-            sed -i -e $prepattern_error -e :a -e $postpattern_error -e 'P;D' "$temp_file""3"
-            awk -v p="$prefix " '{print p $0}' "$temp_file""3" >> $error_file
-
-            eval errant_compare -hyp data/corpus/eval/bea19/bea19.dev.en.$1.hyp.m2 -ref data/corpus/eval/bea19/gold/bea19.dev.en.m2 -ds > "$temp_file""4"
-            sed -i -e $prepattern_results -e :a -e $postpattern_results -e 'P;D' "$temp_file""4"
-            awk -v p="$prefix " '{print p $0}' "$temp_file""4" >> $results_file
-
-            rm "$temp_file"*
-        fi
-    fi
-
 fi
